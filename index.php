b3c89beaececf509f876f22ff4181b1a2250e790
yourname <fermaglad2@gmail.com>
2019-09-29 16:03:43 +0300

    add start

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>calculatorPHP</title>
+     <link rel="stylesheet" href='css/style.css'>
+</head>
+<body>
+<h1>Calculator on PHP</h1> 
+
+
+<h2>Result:</h2>
+<div class="calc">
+
+<p> Attempts done:  <?php  ?></p>
+
+<label for="first">First number:</label>
+<input type="number" class="first">
+<div class="radio">
+<input type="radio" name="radio">
+<input type="radio" name="radio">
+<input type="radio" name="radio">
+<input type="radio" name="radio">
+</div>
+
+
+</div>
+
+
+
+
+
+</body>
+</html>
